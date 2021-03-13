df52cee32efca32b63375a5c573754f44a709271
TYD1127 <2677243689@qq.com>
2021-03-13 19:15:48 +0800

    App获取教室

--- src/main/java/com/winsoar/cams/controller/BuildingController.java
+++ src/main/java/com/winsoar/cams/controller/BuildingController.java
@@ -3,13 +3,14 @@ package com.winsoar.cams.controller;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.winsoar.cams.entity.Building;
-import com.winsoar.cams.pojo.Person;
 import com.winsoar.cams.service.BuildingService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -53,15 +54,40 @@ public class BuildingController {
         JSONArray jsonObject = (JSONArray) JSON.toJSON(maps);
         return jsonObject;
     }
+
     @RequestMapping("/toCkjs")
-    public String toCkjs(String Bid,Model model){
-        List<Map<String,Object>> classroomlist=buildingService.toCkjs(Bid);
-        model.addAttribute("jsinfo",classroomlist);
+    public String toCkjs(String Bid, Model model) {
+        List<Map<String, Object>> classroomlist = buildingService.toCkjs(Bid);
+        model.addAttribute("jsinfo", classroomlist);
         return "h-ui/building/editer";
     }
+
+    @RequestMapping("/mgetjs")
+    @ResponseBody
+    public JSONArray mgetjs(String Bid, Model model) {
+        List<Map<String, Object>> classroomlist = buildingService.toCkjs(Bid);
+        JSONArray jsonObject = (JSONArray) JSON.toJSON(classroomlist);
+        return jsonObject;
+    }
+
+    @RequestMapping("/mgetjsinfo")
+    @ResponseBody
+    public JSONArray mgetjsInfo() {
+        ArrayList<HashMap<String, Object>> list = new ArrayList<>();
+        List<Map<String, Object>> maps = buildingService.queryByAll();
+        for (Map<String, Object> map : maps) {
+            HashMap<String, Object> hashMap = new HashMap<>();
+            List<Map<String, Object>> jsInfo = buildingService.toCkjs(map.get("Bid").toString());
+            hashMap.put(map.get("Bname").toString(), jsInfo);
+            list.add(hashMap);
+        }
+        JSONArray jsonObject = (JSONArray) JSON.toJSON(list);
+        return jsonObject;
+    }
+
     @RequestMapping("/totjjs")
-    public String totjjs(String Bid,Model model){
-        model.addAttribute("jxlid",Bid);
+    public String totjjs(String Bid, Model model) {
+        model.addAttribute("jxlid", Bid);
         return "h-ui/building/insert";
     }
 
